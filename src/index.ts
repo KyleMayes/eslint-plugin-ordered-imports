515bb8f3370828753a5c30877cd6fad6bea11634
Kyle Mayes <kyle@mayeses.com>
2020-04-25 14:51:14 -0400

    Add recommended configuration

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,32 @@
 
 import { rule } from "./rule";
 
-export const rules = { "ordered-imports": rule };
+module.exports = {
+  rules: { "ordered-imports": rule },
+  configs: {
+    recommended: {
+      plugins: ["ordered-imports"],
+      rules: {
+        "ordered-imports/ordered-imports": [
+          "error",
+          {
+            "declaration-ordering": [
+              "type",
+              {
+                ordering: ["side-effect", "default", "namespace", "destructured"],
+                secondaryOrdering: ["name", "lowercase-last"],
+              },
+            ],
+            "specifier-ordering": "lowercase-last",
+            "group-ordering": [
+              { name: "project root", match: "^@", order: 20 },
+              { name: "parent directories", match: "^\\.\\.", order: 30 },
+              { name: "current directory", match: "^\\.", order: 40 },
+              { name: "third-party", match: ".*", order: 10 },
+            ],
+          },
+        ],
+      },
+    },
+  },
+};
